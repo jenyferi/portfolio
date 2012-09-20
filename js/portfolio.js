8e1368e456711697d2954331ccc74b65e2b14c0f
jenyferi <jenyferi@gmail.com>
2012-09-19 19:08:58 -0700

    Added SWF loading using SWFObject.js. Adjusted vertical positioning.

---
+++ js/portfolio.js
@@ -0,0 +1,2 @@
+swfobject.embedSWF("http://jennylin.s3.amazonaws.com/portfolio/images/projects/haagen/haagen_five.swf", "haagen1", "300", "250", "9.0.0", "js/expressInstall.swf");
+swfobject.embedSWF("http://jennylin.s3.amazonaws.com/portfolio/images/projects/haagen/haagen_five2.swf", "haagen2", "720", "90", "9.0.0", "js/expressInstall.swf");
