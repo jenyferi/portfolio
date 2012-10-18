a041c114b3813a3df563325b0600327656ec35b3
jenyferi <jenyferi@gmail.com>
2012-10-18 12:13:32 -0700

    making IE fixes

--- js/bubble.js
+++ js/bubble.js
@@ -1,4 +1,4 @@
-var r = 960,
+var r = 400,
     format = d3.format(",d"),
     fill = d3.scale.category20c();
 
@@ -16,7 +16,7 @@ d3.json("./js/bubbledata.json", function(json) {
   var node = vis.selectAll("g.node")
       .data(bubble.nodes(classes(json))
       .filter(function(d) { return !d.children; }))
-    .enter().append("g")
+      .enter().append("g")
       .attr("class", "node")
       .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
 
@@ -25,19 +25,73 @@ d3.json("./js/bubbledata.json", function(json) {
 
   node.append("circle")
       .attr("r", function(d) { return d.r; })
-      .style("fill", function(d) { return fill(d.packageName); });
+      //.style("fill", function(d) { return fill(d.packageName); });
+      .style("fill", function(d) { 
+        if (d.parent){
+          return getFill(d); 
+        }
+      })
+      .on("mouseover", makeMeBig)
+      .on("mouseout", makeMeSmall);
 
-  node.append("text")
+  //addText();
+
+  function addText(){
+    node.append("text")
       .attr("text-anchor", "middle")
       .attr("dy", ".3em")
-      .text(function(d) { return d.className.substring(0, d.r / 3); });
-
-  node.hover(
-    function(){$("text").fadeOut()},
-    function(){$("text").fadeOut()}
-  );
-
+      //.text(function(d) { return d.className.substring(0, d.r / 3); });
+      .text(function(d) { return d.className; });
+  }
 });
+
+function makeMeBig() {
+  d3.select(this)
+    .transition()
+    .delay(0)
+    .duration(1000)
+    .attr("r", 80);
+}
+
+function makeMeSmall() {
+  d3.select(this)
+    .transition()
+    .delay(0)
+    .duration(1000)
+    .attr("r", 10);
+}
+
+function fadeInMyText(n) {
+  
+}
+
+function getFill(n) {
+  /*0/6 = 9ec0bc //green
+  1/6 = 9eb2b0
+  2/6 = b0a8ad
+  3/6 = c29da8
+  4/6 = d593a5
+  5/6 = e588a1
+  6/6 = e98a9f //pink */
+
+  if (n.packageName == "1"){
+    return d3.rgb("#9ec0bc"); 
+  }else if (n.packageName == "2"){
+    return d3.rgb("#9eb2b0");
+  }else if (n.packageName == "3"){
+    return d3.rgb("#b0a8ad");
+  }else if (n.packageName == "4"){
+    return d3.rgb("#c29da8");
+  }else if (n.packageName == "5"){
+    return d3.rgb("#d593a5");
+  }else if (n.packageName == "6"){
+    return d3.rgb("#e588a1");
+  }else if (n.packageName == "7"){
+    return d3.rgb("#e98a9f");
+  }else {
+    return d3.rgb("#c29da8");
+  }
+}
 
 // Returns a flattened hierarchy containing all leaf nodes under the root.
 function classes(root) {
--- js/portfolio.js
+++ js/portfolio.js
@@ -1,14 +1,50 @@
 $(document).ready(function () {
+
+    var phillips_html = '<div id="phillips0"><img class="projectimg" src="img/projects/phillips0.png"></div><div id="phillips1"><img class="projectimg" src="img/projects/phillips1.png"></div><div id="phillips2"><img class="projectimg" src="img/projects/phillips2.png"></div><div id="phillips3"><img class="projectimg" src="img/projects/phillips3.png"></div>';
+
+    var sears_html = '<div id="sears0"><img class="projectimg" src="img/projects/sears0.jpg"></div><div id="sears1"><img class="projectimg" src="img/projects/sears1.jpg"></div><div id="sears2"><img class="projectimg" src="img/projects/sears2.jpg"></div><div id="sears3"><img class="projectimg" src="img/projects/sears3.jpg"></div><div id="sears4"><img class="projectimg" src="img/projects/sears4.jpg"></div><div id="sears5"><img class="projectimg" src="img/projects/sears5.jpg"></div><div id="sears6"><img class="projectimg" src="img/projects/sears6.jpg"></div><div id="sears7"><img class="projectimg" src="img/projects/sears7.jpg"></div><div id="sears8"><img class="projectimg" src="img/projects/sears8.jpg"></div>';
+
+    var android_html = '<div id="android0"><img class="projectimg" src="img/projects/android0.jpg"></div><div id="android1"><img class="projectimg" src="img/projects/android1.jpg"></div><div id="android2"><img class="projectimg" src="img/projects/android2.jpg"></div><div id="android3"><img class="projectimg" src="img/projects/android3.jpg"></div><div id="android4"><img class="projectimg" src="img/projects/android4.jpg"></div><div id="android5"><img class="projectimg" src="img/projects/android5.jpg"></div>';
+
+    var wildswans_html = '<iframe src="http://player.vimeo.com/video/35960103?title=0&byline=0&portrait=0&color=d46fab" width="720" height="396" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>';
+
+    var freise_html = '<div id="freise0"><img class="projectimg" src="img/projects/freise0.jpg"></div><div id="freise1"><img class="projectimg" src="img/projects/freise1.jpg"></div><div id="freise2"><img class="projectimg" src="img/projects/freise2.jpg"></div><div id="freise3"><img class="projectimg" src="img/projects/freise3.jpg"></div>';
+
+    var kayak_html = '<div id="kayak0"><img class="projectimg" src="img/projects/kayak0.png"></div><div id="kayak1"><img class="projectimg" src="img/projects/kayak1.png"></div><div id="kayak2"><img class="projectimg" src="img/projects/kayak2.png"></div><div id="kayak3"><img class="projectimg" src="img/projects/kayak3.png"></div><div id="kayak4"><img class="projectimg" src="img/projects/kayak4.png"></div><div id="kayak5"><img class="projectimg" src="img/projects/kayak5.png"></div>';
+
+    var tree_html = '<iframe src="http://player.vimeo.com/video/35948137?title=0&byline=0&portrait=0&color=d46fab&autoplay=1&loop=1" width="400" height="400" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>';
+
+    var pirates_html = '<img src="img/pirates.png" alt="pirates">';
+
+    $("#phillips-imgs").html(phillips_html);
+    $("#sears-imgs").html(sears_html);
+    $("#android-imgs").html(android_html);
+    $("#wildswans1").html(wildswans_html);
+    $("#freise-imgs").html(freise_html);
+    $("#kayak-imgs").html(kayak_html);
+    $("#tree1").html(tree_html);
+    $("#pirates1").html(pirates_html);
+
+    $(function() {
+		var zIndexNumber = 1000;
+		$('div').each(function() {
+			$(this).css('zIndex', zIndexNumber);
+			zIndexNumber -= 10;
+		});
+	});
 	
 	//adjusting text shadows based on browser
 	if($.browser.chrome){
 		$("#intro-top").css("color", "#545454");
 		$("#intro-top").css("text-shadow", "0.015em 0.02em 0.09em #888888");
 	}
-
-	if($.browser.mozilla){
+	else if($.browser.mozilla){
 		$("#intro-top").css("text-shadow", "0.005em 0.02em 0.04em #666666");
 	}
+
+	/*if(jQuery.browser.mobile){
+		$("#navbar ul").css("font-weight", "400");
+	}*/
 
 	//logo 
 	$("#ba").fadeOut(0);
@@ -26,6 +62,9 @@ $(document).ready(function () {
 	for (var i=1; i<numProjects; i++){
 		$("#project" + i).fadeOut(0); //fade out all except first
 	}
+
+	//starting project
+	projectImages("phillips", 4);
 
 	$(".button").click(function() {
 		var currButtonID = $(this).attr('id');
@@ -49,13 +88,13 @@ $(document).ready(function () {
 
 		  		$("#numbers").html("" + (currProject+1) + " / " + numProjects);
 
-		  		if (currProject == 3) {
+		  		if (currProject == 4) {
 		  			projectImages("sears", 9);
 		  		}else if (currProject == 8){
 		  			projectImages("kayak", 6);
-		  		}else if (currProject == 5){
+		  		}else if (currProject == 3){
 		  			projectImages("android", 6);
-		  		}else if (currProject == 1){ 
+		  		}else if (currProject == 0){ 
 		  			projectImages("phillips", 4);
 		  		}else if (currProject == 7){ 
 		  			projectImages("freise", 4);
@@ -105,7 +144,7 @@ $(document).ready(function () {
 
 		var k = 0;
 		var changeTime = fadeTime*10;
-		var delayTime = fadeTime*3;
+		var delayTime = fadeTime*2;
 		var timesrun = 0;
 		var shouldFlip = true;
 
@@ -178,11 +217,11 @@ $(document).ready(function () {
 		function(){this.src = this.src.replace("img/resume_circle_hover.png","img/resume_circle.png");
 	});
 
-	swfobject.embedSWF("http://jennylin.s3.amazonaws.com/portfolio/images/projects/haagen/haagen_five.swf", "haagen1", "300", "250", "9.0.0", "js/expressInstall.swf");
-	swfobject.embedSWF("http://jennylin.s3.amazonaws.com/portfolio/images/projects/haagen/haagen_five2.swf", "haagen2", "720", "90", "9.0.0", "js/expressInstall.swf");
-	swfobject.embedSWF("http://jennylin.s3.amazonaws.com/portfolio/images/projects/quaker/quaker_apple.swf", "quaker1", "300", "250", "9.0.0", "js/expressInstall.swf");
-	swfobject.embedSWF("http://jennylin.s3.amazonaws.com/portfolio/images/projects/quaker/quaker_coconut.swf", "quaker2", "300", "250", "9.0.0", "js/expressInstall.swf");
-	swfobject.embedSWF("http://jennylin.s3.amazonaws.com/portfolio/images/projects/castle/scene1.swf", "castle1", "680", "425", "9.0.0", "js/expressInstall.swf");
-	swfobject.embedSWF("http://jennylin.s3.amazonaws.com/portfolio/images/projects/forest/scene2.swf", "forest1", "680", "425", "9.0.0", "js/expressInstall.swf");
+	swfobject.embedSWF("flash/haagen_five.swf", "haagen1", "300", "250", "9.0.0", "js/expressInstall.swf");
+	swfobject.embedSWF("flash/haagen_five2.swf", "haagen2", "720", "90", "9.0.0", "js/expressInstall.swf");
+	swfobject.embedSWF("flash/quaker_apple.swf", "quaker1", "300", "250", "9.0.0", "js/expressInstall.swf");
+	swfobject.embedSWF("flash/quaker_coconut.swf", "quaker2", "300", "250", "9.0.0", "js/expressInstall.swf");
+	swfobject.embedSWF("flash/castle.swf", "castle1", "680", "425", "9.0.0", "js/expressInstall.swf");
+	swfobject.embedSWF("flash/forest.swf", "forest1", "680", "425", "9.0.0", "js/expressInstall.swf");
 	//swfobject.embedSWF("http://jennylin.s3.amazonaws.com/portfolio/images/projects/twitter/twitter.swf", "rmgallery1", "120", "450", "9.0.0", "js/expressInstall.swf");
 });
