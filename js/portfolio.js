ef8180c908d477730b617de429b838beb705f1f8
jenyferi <jenyferi@gmail.com>
2012-10-31 02:27:12 -0700

    added hashtags for each project so they have their own URL

--- js/portfolio.js
+++ js/portfolio.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
 
     var android_html = '<div id="android0"><img class="projectimg" src="img/projects/android0.jpg"></div><div id="android1"><img class="projectimg" src="img/projects/android1.jpg"></div><div id="android2"><img class="projectimg" src="img/projects/android2.jpg"></div><div id="android3"><img class="projectimg" src="img/projects/android3.jpg"></div><div id="android4"><img class="projectimg" src="img/projects/android4.jpg"></div><div id="android5"><img class="projectimg" src="img/projects/android5.jpg"></div>';
 
-    var wildswans_html = '<iframe id="player1" src="http://player.vimeo.com/video/35960103?title=0&byline=0&portrait=0&color=d46fab" width="720" height="396" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>';
+    var wildswans_html = '<iframe id="player1" src="http://player.vimeo.com/video/35960103?title=0&byline=0&portrait=0&color=d46fab&player_id=player1&api=1" width="720" height="396" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>';
 
     var freise_html = '<div id="freise0"><img class="projectimg" src="img/projects/freise0.jpg"></div><div id="freise1"><img class="projectimg" src="img/projects/freise1.jpg"></div><div id="freise2"><img class="projectimg" src="img/projects/freise2.jpg"></div><div id="freise3"><img class="projectimg" src="img/projects/freise3.jpg"></div>';
 
@@ -50,12 +50,51 @@ $(document).ready(function () {
 	}*/
 
 	//logo 
-	$("#ba").fadeOut(0);
-
 	/*$("#logo").hover(
 		function(){ $("#ba").fadeIn(200); },
 		function(){ $("#ba").fadeOut(200); }
 	);*/
+
+	//communicate with vimeo player
+	var iframe = $('#player1')[0];
+    var player = $f(iframe);
+    var is_ws_playing = false;
+
+	// When the player is ready, add listeners for pause, finish, and playProgress
+	player.addEvent('ready', function() {
+	    //console.log("ready");
+	    
+	    player.addEvent('pause', onPause);
+	    player.addEvent('finish', onFinish);
+	    player.addEvent('play', onPlay);
+	});
+
+	// Call the API when a button is pressed
+	$('button').bind('click', function() {
+	    player.api($(this).text().toLowerCase());
+	});
+
+	function onPause(id) {
+		is_ws_playing = false;
+	    //console.log("paused");
+	}
+
+	function onFinish(id) {
+		is_ws_playing = false;
+	    //console.log("finished");
+	}
+
+	function onPlay(id) {
+		is_ws_playing = true;
+		console.log(player);
+	    //console.log("play");
+	}
+
+	function pausePlayer(){
+		console.log("in is_ws_playing" + player);
+		player.api("pause");
+		//player.pause();
+	}
 
 	//project swap and fade logic
 	var numProjects = 12;
@@ -69,44 +108,88 @@ $(document).ready(function () {
 	//starting project
 	projectImages("phillips", 4);
 
+	var hashname = "project";
+	var prevProject = 1; //first project
+	var newProject = 1;
+
 	$(".button").click(function() {
 		var currButtonID = $(this).attr('id');
-  		$("#project" + currProject).fadeOut(fadeTime, 
+
+		updateCurrentProject(currButtonID);
+		changeHash();
+	});
+
+	function changeHash() {
+		location.hash = hashname + (currProject+1);
+	}
+
+	function updateCurrentProject(currButtonID) {
+		currProject = newProject-1;
+
+		if (currButtonID == "button-right"){
+  			currProject++;
+  		}else if (currButtonID == "button-left"){
+  			currProject--;
+  		}
+
+		if (currProject == numProjects){
+			currProject = 0;
+		}
+
+		if (currProject < 0){
+			currProject = numProjects-1;
+		}
+	}
+
+	// hashchange jquery library
+	$(window).hashchange( function(){
+		var hashlength = location.hash.length;
+		var difference = hashlength - hashname.length;
+		prevProject = newProject;
+		newProject = location.hash.substring(hashlength - difference + 1);
+
+		//if someone types in a wrong project #
+		if (newProject > numProjects || newProject <= 0){
+			newProject = 1;
+		}
+
+		loadProject(newProject); 
+	})
+
+	$(window).hashchange();
+
+
+	function loadProject(hashID){
+		hashID = hashID - 1;
+		prevProject = prevProject - 1;
+		console.log("previous project = " + prevProject);
+		$("#project" + prevProject).fadeOut(fadeTime, 
   			function(){
-	  			if (currButtonID == "button-right"){
-		  			currProject++;
-		  		}else if (currButtonID == "button-left"){
-		  			currProject--;
-		  		}
-
-				if (currProject == numProjects){
-					currProject = 0;
+				if (is_ws_playing){
+					pausePlayer();
+					is_ws_playing = false;
 				}
 
-				if (currProject < 0){
-					currProject = numProjects-1;
-				}
+		  		$("#project" + hashID).fadeIn(fadeTime);
 
-		  		$("#project" + currProject).fadeIn(fadeTime);
+		  		$("#numbers").html("" + (hashID+1) + " / " + numProjects);
 
-		  		$("#numbers").html("" + (currProject+1) + " / " + numProjects);
-
-		  		if (currProject == 4) {
+		  		if (hashID == 4) {
 		  			projectImages("sears", 9);
-		  		}else if (currProject == 8){
+		  		}else if (hashID == 8){
 		  			projectImages("kayak", 6);
-		  		}else if (currProject == 3){
+		  		}else if (hashID == 3){
 		  			projectImages("android", 6);
-		  		}else if (currProject == 0){ 
+		  		}else if (hashID == 0){ 
 		  			projectImages("phillips", 4);
-		  		}else if (currProject == 7){ 
+		  		}else if (hashID == 7){ 
 		  			projectImages("freise", 4);
 		  		}else {
 		  			removeImageDots();
 		  		}
   			}
   		);
-	});
+	}
 
 	/*$("#button-right").click(function() {
 		$(this).effect("bounce", { direction:'left', times:5 }, 200);
@@ -140,36 +223,6 @@ $(document).ready(function () {
           function(){this.src = this.src.replace("img/history5.png","img/google.png");
      });*/
 	////////////////////////////////////////////////////////////////////////////////////////
-
-	//communicate with vimeo player
-	var iframe = $('#player1')[0];
-    var player = $f(iframe);
-
-	// When the player is ready, add listeners for pause, finish, and playProgress
-	player.addEvent('ready', function() {
-	    console.log("ready");
-	    
-	    player.addEvent('pause', onPause);
-	    player.addEvent('finish', onFinish);
-	    player.addEvent('playProgress', onPlayProgress);
-	});
-
-	// Call the API when a button is pressed
-	$('button').bind('click', function() {
-	    player.api($(this).text().toLowerCase());
-	});
-
-	function onPause(id) {
-	    console.log("paused");
-	}
-
-	function onFinish(id) {
-	    console.log("finished");
-	}
-
-	function onPlayProgress(data, id) {
-	    console.log(data.seconds + "s played");
-	}
 
 	//projects
 	function projectImages(projectName, numImages){
@@ -262,7 +315,7 @@ $(document).ready(function () {
 					k++;
 				}
 			}
-			
+
 			$("#dot" + k).css("color", "#e98a9f");
 
 			$("#" + projectName + k).delay(delayTime).fadeIn(fadeTime, 
